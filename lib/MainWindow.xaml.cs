3c476fc30750e0e2b312db0e353fd15667f04d9e
Baskaev Daniel <baskaev.danyya@gmail.com>
2025-03-06 16:45:42 +0300

    100% project

--- lib/MainWindow.xaml.cs
+++ lib/MainWindow.xaml.cs
@@ -12,15 +12,49 @@ namespace lib
         {
             InitializeComponent();
             _context = new LibraryContext();
+            LoadAuthorsAndGenres();
             LoadBooks();
+        }
+
+        private void LoadAuthorsAndGenres()
+        {
+            AuthorFilterComboBox.ItemsSource = _context.Authors.ToList();
+            GenreFilterComboBox.ItemsSource = _context.Genres.ToList();
         }
 
         private void LoadBooks()
         {
-            BooksGrid.ItemsSource = _context.Books
+            var query = _context.Books
                 .Include(b => b.Author)
                 .Include(b => b.Genre)
-                .ToList();
+                .AsQueryable();
+
+            // Применение поиска по названию
+            if (!string.IsNullOrEmpty(SearchTextBox.Text))
+            {
+                query = query.Where(b => b.Title.Contains(SearchTextBox.Text));
+            }
+
+            // Применение фильтра по автору
+            if (AuthorFilterComboBox.SelectedItem != null)
+            {
+                var selectedAuthor = (Author)AuthorFilterComboBox.SelectedItem;
+                query = query.Where(b => b.AuthorId == selectedAuthor.Id);
+            }
+
+            // Применение фильтра по жанру
+            if (GenreFilterComboBox.SelectedItem != null)
+            {
+                var selectedGenre = (Genre)GenreFilterComboBox.SelectedItem;
+                query = query.Where(b => b.GenreId == selectedGenre.Id);
+            }
+
+            BooksGrid.ItemsSource = query.ToList();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadBooks();
         }
 
         private void AddBook_Click(object sender, RoutedEventArgs e)
@@ -33,23 +67,27 @@ namespace lib
                 LoadBooks();
             }
         }
+
         private void ManageAuthors_Click(object sender, RoutedEventArgs e)
         {
             var manageAuthorsWindow = new ManageAuthorsWindow();
             manageAuthorsWindow.ShowDialog();
 
             // После закрытия окна управления авторами обновляем список авторов
+            LoadAuthorsAndGenres();
             LoadBooks();
         }
+
         private void ManageGenres_Click(object sender, RoutedEventArgs e)
         {
             var manageGenresWindow = new ManageGenresWindow();
             manageGenresWindow.ShowDialog();
-            
+
+            // После закрытия окна управления жанрами обновляем список жанров
+            LoadAuthorsAndGenres();
             LoadBooks();
         }
 
-        
         private void EditBook_Click(object sender, RoutedEventArgs e)
         {
             var selectedBook = BooksGrid.SelectedItem as Book;
